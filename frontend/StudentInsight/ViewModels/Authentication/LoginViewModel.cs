956f30f45d8df1fd4849853e8d0b58f456b47e49
Talha Ahmad <talha.ahmad.tech@gmail.com>
2026-03-18 11:56:39 +0500

    Create Auth Views and add bindings.
    I created authentication views for login and register the users. I also added some bindings and set "startup" window to auth.

---
+++ frontend/StudentInsight/ViewModels/Authentication/LoginViewModel.cs
@@ -0,0 +1,53 @@
+﻿using StudentInsight.Helpers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace StudentInsight.ViewModels.Authentication
+{
+    public class LoginViewModel
+    {
+        private readonly AuthViewModel viewModel;
+
+        public string Email { get; set; }
+
+        public ICommand LoginCommand { get; }
+        public ICommand GoToRegisterCommand { get; }
+
+        public LoginViewModel(AuthViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+
+            LoginCommand = new RelayCommand(ExecuteLogin);
+            GoToRegisterCommand = new RelayCommand(_ => viewModel.ShowRegister());
+        }
+
+        private void ExecuteLogin(object parameter)
+        {
+            var passwordBox = parameter as PasswordBox;
+            string password = passwordBox!.Password;
+
+            // TODO: Call API (Auth Service)
+
+            bool isSuccess = true;
+
+            if (isSuccess)
+            {
+                // Open the main window
+                var mainWindow = new MainWindow();
+                mainWindow.Show();
+
+                // Close the auth window
+                Application.Current.Windows
+                    .OfType<Window>()
+                    .Single(w => w is AuthWindow)
+                    .Close();
+            }
+        }
+
+        private void GoToRegister(object obj)
+        {
+            viewModel.ShowRegister();
+        }
+    }
+}
---
+++ frontend/StudentInsight/ViewModels/Authentication/RegisterViewModel.cs
@@ -0,0 +1,41 @@
+﻿using StudentInsight.Helpers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace StudentInsight.ViewModels.Authentication
+{
+    public class RegisterViewModel
+    {
+        private readonly AuthViewModel viewModel;
+
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public ICommand RegisterCommand { get; }
+        public ICommand GoToLoginCommand { get; }
+
+        public RegisterViewModel(AuthViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+
+            RegisterCommand = new RelayCommand(ExecuteRegister);
+            GoToLoginCommand = new RelayCommand(_ => viewModel.ShowLogin());
+        }
+
+        private void ExecuteRegister(object parameter)
+        {
+            var passwordBox = parameter as PasswordBox;
+            string password = passwordBox!.Password;
+
+            // TODO: API call
+            MessageBox.Show($"Register:\n{Username}\n{Email}\n{Password}");
+        }
+
+        private void GoToLogin(object obj)
+        {
+            viewModel.ShowLogin();
+        }
+    }
+}
